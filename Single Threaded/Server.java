f24807183ae51efab7dcce1e8e149010fe3323ee
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-11-18 01:22:22 +0530

    multithreading

--- Single Threaded/Server.java
+++ Single Threaded/Server.java
@@ -1,12 +1,18 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server{
-    public void run(){
+    public void run() throws IOException{
         int port=8010;
         ServerSocket socket= new ServerSocket(port);
         socket.setToTimeout(10000);
         while(true){
             try{
-                System.out.println('Server is running on'+port);
+                System.out.println("Server is running on"+port);
                 Socket acceptedConnection=socket.accept();
+                System.out.println("Socket connection accepted"+acceptedConnection.getRemoteSocketAddress());
             }catch(Exception ex){
                 ex.printStackTrace();
             }
